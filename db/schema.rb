d144da5f015a09d1a363e0ad7eabd75dfbf33e7d
Hashrocket Workstation <dev@hashrocket.com>
2016-12-11 00:51:05 -0500

    Add authem migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210220525) do
+ActiveRecord::Schema.define(version: 20161211054959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authem_sessions", force: :cascade do |t|
+    t.string   "role",                    null: false
+    t.string   "subject_type",            null: false
+    t.integer  "subject_id",              null: false
+    t.string   "token",        limit: 60, null: false
+    t.datetime "expires_at",              null: false
+    t.integer  "ttl",                     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["expires_at", "subject_type", "subject_id"], name: "index_authem_sessions_subject", using: :btree
+    t.index ["expires_at", "token"], name: "index_authem_sessions_on_expires_at_and_token", unique: true, using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                           null: false
+    t.string   "password_digest",                 null: false
+    t.string   "password_reset_token", limit: 60, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "workshops", force: :cascade do |t|
     t.string   "title"
