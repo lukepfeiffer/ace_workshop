4cfeebaa45112dbd335c4b6328c1ab6c848f4d7b
Hashrocket Workstation <dev@hashrocket.com>
2016-12-26 11:50:59 -0500

    Fix published scope

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,5 +1,5 @@
 class Workshop < ActiveRecord::Base
   scope :pending, -> {where("publish_date > ?", Date.today)}
-  scope :published, -> {where.not(publish_date: nil)}
+  scope :published, -> {where("publish_date < ?", Date.today)}
   scope :archived, -> {where(publish_date: nil)}
 end
